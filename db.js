a4bf2c1af9a0763c034eca934b23c68c9e9338aa
xlzy <1017866168@qq.com>
2022-05-03 19:34:48 +0800

    feat: 封装数据库操作

--- db.js
+++ db.js
@@ -1,13 +1,38 @@
 const low = require('lowdb');
 
 const fileAsync = require('lowdb/adapters/FileAsync');
+const uuid4 = require('uuid4');
 
 let db;
 
 const createDB = async () => {
   const adapter = new fileAsync('db.json');
   db = await low(adapter);
-  db.defaults({ tasks: [], users: [] }).write();
+  db.defaults({ tasks: [], users: [], orders: [] }).write();
+  db._.mixin({
+    push: (data, values) => {
+      data.push({
+        ...values,
+        id: uuid4(),
+        creatTime: Date.now(),
+        updateTime: Date.now(),
+      });
+    },
+    assign: (data, values) => {
+      Object.assign(data, {
+        ...values,
+        updateTime: Date.now(),
+      });
+    },
+    pagination: (data, page, limit) => {
+      const start = (page - 1) * limit;
+      const end = page * limit;
+      return {
+        list: data.slice(start, end),
+        total: data.length,
+      };
+    },
+  });
 };
 
 const getDB = () => db;
