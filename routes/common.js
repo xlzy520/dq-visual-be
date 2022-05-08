c311a673ab712359b43555936d0f3c1983224b2e
xlz <20794489@qq.com>
2022-05-08 20:44:53 +0800

    修复图片上传

--- routes/common.js
+++ routes/common.js
@@ -8,11 +8,18 @@ router.post('/upload', async (ctx, next) => {
   let path = '/public';
   if (!file) {
     file = files.files;
-    path = 'public\\';
   }
   const filePath = file.filepath;
   const originalname = file.originalname || file.originalFilename;
-  const fileName = filePath.split(path).pop();
+  let fileName
+  if (filePath.includes('\\')) {
+    path = 'public\\';
+    fileName =  '/'+filePath.split(path).pop();
+  } else {
+    fileName = filePath.split(path).pop();
+  }
+  console.log(filePath);
+
   let basicUrl = 'http://localhost:5001'; //为基础路径
   ctx.body = {
     filename: originalname,
